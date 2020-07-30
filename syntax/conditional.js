a2d274d18d34184cacae873d0a689e231473273e
leejaek <67036362+leejaek@users.noreply.github.com>
2020-07-30 14:08:05 +0900

    16. Node.js - 콘솔에서의 입력값

--- syntax/conditional.js
+++ syntax/conditional.js
@@ -1,7 +1,10 @@
+var args = process.argv;
+console.log(args[2]);
+
 console.log('A');
 console.log('B');
 //괄호안에는 true or false 둘 중 하나의 결과값은 반환
-if(true){
+if(args[2] === '1'){
   //true 면 C1이 실행
   console.log('C1');
 } else {
