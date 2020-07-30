d811b9b9e141ae7017ef83777cf7081dc370a08b
leejaek <67036362+leejaek@users.noreply.github.com>
2020-07-30 22:20:31 +0900

    27. App - 함수를 이용해서 정리 정돈하기

--- main.js
+++ main.js
@@ -2,6 +2,34 @@ var http = require('http');
 var fs = require('fs');
 var url = require('url');
 
+function templateHTML(title, list, body){
+  return `
+  <!doctype html>
+  <html>
+  <head>
+    <title>WEB1 - ${title}</title>
+    <meta charset="utf-8">
+  </head>
+  <body>
+    <h1><a href="/">WEB</a></h1>
+    ${list}
+    ${body}
+  </body>
+  </html>
+  `;
+}
+
+function templateList(filelist){
+  var list = '<ul>';
+  var i = 0;
+  while(i < filelist.length){
+    list = list + `<li><a href ="/?id=${filelist[i]}">${filelist[i]}</a></li>`
+    i = i + 1
+  }
+  list = list + '</ul>';
+  return list
+}
+
 var app = http.createServer(function(request,response){
     var _url = request.url;
     var queryData = url.parse(_url, true).query;
@@ -10,63 +38,22 @@ var app = http.createServer(function(request,response){
       if(queryData.id === undefined){
         fs.readdir('./data', function(err, filelist){
             var title = 'Welcome';
-            var description = 'Hello, Node.js'; //title과 description을 수동적으로 정의 -> 위 코드에서 file을 읽어 description을 가져오는 코드는 필요 업어짐
-            var list = '<ul>';
-            var i = 0;
-            while(i < filelist.length){
-              list = list + `<li><a href ="/?id=${filelist[i]}">${filelist[i]}</a></li>`
-              i = i + 1
-            }
-            list = list + '</ul>';
-            var template =
-            `<!doctype html>
-            <html>
-            <head>
-              <title>WEB1 - ${title}</title>
-              <meta charset="utf-8">
-            </head>
-            <body>
-              <h1><a href="/">WEB</a></h1>
-              ${list}
-              <h2>${title}</h2>
-              <p>${description}</p>
-            </body>
-            </html>`
+            var description = 'Hello, Node.js';
+            var list = templateList(filelist);
+            var template = templateHTML(title, list, `<h2>${title}</h2>${description}`);
             response.writeHead(200);
             response.end(template);
         })
       } else {
         fs.readdir('./data', function(err, filelist){
-            var title = 'Welcome';
-            var description = 'Hello, Node.js'; //title과 description을 수동적으로 정의 -> 위 코드에서 file을 읽어 description을 가져오는 코드는 필요 업어짐
-            var list = '<ul>';
-            var i = 0;
-            while(i < filelist.length){
-              list = list + `<li><a href ="/?id=${filelist[i]}">${filelist[i]}</a></li>`
-              i = i + 1
-            }
-            list = list + '</ul>';
-        fs.readFile(`data/${queryData.id}`, 'utf8',
-        function(err, description){
-          var title = queryData.id;
-          var template =
-          `<!doctype html>
-          <html>
-          <head>
-            <title>WEB1 - ${title}</title>
-            <meta charset="utf-8">
-          </head>
-          <body>
-            <h1><a href="/">WEB</a></h1>
-            ${list}
-            <h2>${title}</h2>
-            <p>${description}</p>
-          </body>
-          </html>`
-          response.writeHead(200);
-          response.end(template);
+          fs.readFile(`data/${queryData.id}`, 'utf8', function(err, description){
+            var title = queryData.id;
+            var list = templateList(filelist);
+            var template = templateHTML(title, list, `<h2>${title}</h2>${description}`);
+            response.writeHead(200);
+            response.end(template);
+          });
         });
-      });
       }
     } else {
       response.writeHead(404);
