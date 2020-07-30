5e84f50d7f90cab962432e90b8fdd8f30bd2628f
leejaek <67036362+leejaek@users.noreply.github.com>
2020-07-30 13:20:36 +0900

    13. JavaScript - 비교 연산자

---
+++ syntax/comparison.js
@@ -0,0 +1,12 @@
+//아래 console.log의 (1+1) 과 (2)는 같은 표기
+console.log(1+1); //2
+console.log(2); //2
+
+console.log(1==1); //true
+console.log(2==1); //false
+console.log(1>2); //false
+console.log(1<2); //true
+console.log(1===1); //true //다른 이유가 없다면 === 세개를 사용
+console.log(2===1); //false
+
+name === 1;
