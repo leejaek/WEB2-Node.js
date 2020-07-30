0aa50331da6025276a59c4442ee575d8e7105863
leejaek <67036362+leejaek@users.noreply.github.com>
2020-07-30 18:33:55 +0900

    23. App - 글목록 출력하기

--- main.js
+++ main.js
@@ -1,63 +1,54 @@
 var http = require('http');
 var fs = require('fs');
-// web app은 url을 require함 - 'url' Module을 사용함 - 'url' Module은 var url 변수를 통해 사용
-// 아래있던 url 변수값은 _url로 변경하여 겹치지 않도록
 var url = require('url');
 
 var app = http.createServer(function(request,response){
     var _url = request.url;
-    // queryData에는
     var queryData = url.parse(_url, true).query;
-    //queryString이 주소에 있어도 queryString을 제외한 path 만을 보여
     var pathname = url.parse(_url, true).pathname;
-    //title 변수에 queryData.id를 넣어서 아래 template coded 에서 활용
-    // '/' url에 대한 조건문을 걸어서 아래 template에서 <h1>WEB</h1> 실행 시 title = Welcome으로 변경되도록 함
-    /*if(_url == '/'){
-      title = 'Welcome';
-    }
-    if(_url == '/favicon.ico'){
-      response.writeHead(404);
-      response.end();
-      return;
-    }*/ //사용 안함
-
     if(pathname=== '/'){
       if(queryData.id === undefined){
-        fs.readFile(`data/${queryData.id}`, 'utf8',
-        function(err, description){
-          var title = 'Welcome';
-          var description = 'Hello, Node.js';
-          // 템플릿 작성
-          var template =
-          `<!doctype html>
-          <html>
-          <head>
-            <title>WEB1 - ${title}</title>
-            <meta charset="utf-8">
-          </head>
-          <body>
-            <h1><a href="/">WEB</a></h1>
-            <ol>
-              <li><a href="/?id=HTML">HTML</a></li>
-              <li><a href="/?id=CSS">CSS</a></li>
-              <li><a href="/?id=JavaScript">JavaScript</a></li>
-            </ol>
-            <h2>${title}</h2>
-            <p>${description}</p>
-          </body>
-          </html>`
-          //writeHead(): 웹브라우저가 웹서버에 접속 - 응답 시 잘 되었는지, 페이지가 옮겨졌는지 등을 중요한 정보를 기계-기계 간 통신하기 위한 코드
-          //200: 파일을 성공적으로 전송함
-          //404: 파일을 찾을 수 없음
-          response.writeHead(200);
-          //사용자가 접속한 페이지에 따라서 읽어들임
-          response.end(template);
-        });
+        fs.readdir('./data', function(err, filelist){
+            var title = 'Welcome';
+            var description = 'Hello, Node.js'; //title과 description을 수동적으로 정의 -> 위 코드에서 file을 읽어 description을 가져오는 코드는 필요 업어짐
+            var list = '<ul>';
+            var i = 0;
+            while(i < filelist.length){
+              list = list + `<li><a href ="/?id=${filelist[i]}">${filelist[i]}</a></li>`
+              i = i + 1
+            }
+            list = list + '</ul>';
+            var template =
+            `<!doctype html>
+            <html>
+            <head>
+              <title>WEB1 - ${title}</title>
+              <meta charset="utf-8">
+            </head>
+            <body>
+              <h1><a href="/">WEB</a></h1>
+              ${list}
+              <h2>${title}</h2>
+              <p>${description}</p>
+            </body>
+            </html>`
+            response.writeHead(200);
+            response.end(template);
+        })
       } else {
+        fs.readdir('./data', function(err, filelist){
+            var title = 'Welcome';
+            var description = 'Hello, Node.js'; //title과 description을 수동적으로 정의 -> 위 코드에서 file을 읽어 description을 가져오는 코드는 필요 업어짐
+            var list = '<ul>';
+            var i = 0;
+            while(i < filelist.length){
+              list = list + `<li><a href ="/?id=${filelist[i]}">${filelist[i]}</a></li>`
+              i = i + 1
+            }
+            list = list + '</ul>';
         fs.readFile(`data/${queryData.id}`, 'utf8',
         function(err, description){
           var title = queryData.id;
-          // 템플릿 작성
           var template =
           `<!doctype html>
           <html>
@@ -67,11 +58,7 @@ var app = http.createServer(function(request,response){
           </head>
           <body>
             <h1><a href="/">WEB</a></h1>
-            <ol>
-              <li><a href="/?id=HTML">HTML</a></li>
-              <li><a href="/?id=CSS">CSS</a></li>
-              <li><a href="/?id=JavaScript">JavaScript</a></li>
-            </ol>
+            ${list}
             <h2>${title}</h2>
             <p>${description}</p>
           </body>
@@ -79,10 +66,10 @@ var app = http.createServer(function(request,response){
           response.writeHead(200);
           response.end(template);
         });
+      });
       }
     } else {
       response.writeHead(404);
-      //사용자가 접속한 페이지에 따라서 읽어들임
       response.end('Not Found');
     }
 });
