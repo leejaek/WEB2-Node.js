ee0de17f88346a71fa8640415d5d79affc82fef6
leejaek <67036362+leejaek@users.noreply.github.com>
2020-07-30 20:04:58 +0900

    JavaScript - 함수의 입력

---
+++ syntax/function2.js
@@ -0,0 +1,13 @@
+//JavaScript의 내장 함수 (math.round - 반올림)
+console.log(Math.round(1.6)); //2
+console.log(Math.round(1.4)); //1
+
+//함수는 입력값을 가짐
+//입력값을 받기 위해서는 매개변수가 필요하다.
+
+function sum(first, second){ //Parameter Variable: 매개변수 (입력값요소를 받아 함수에 전달)
+  console.log(first); //2
+  console.log(first+second); //6
+}
+
+sum(2,4) //argument: 입력값 요소
