f178b69138943756bd1066ad9d07d88f384e95bb
leejaek <67036362+leejaek@users.noreply.github.com>
2020-07-30 13:53:32 +0900

    15. JavaScript - 조건문

---
+++ syntax/conditional.js
@@ -0,0 +1,11 @@
+console.log('A');
+console.log('B');
+//괄호안에는 true or false 둘 중 하나의 결과값은 반환
+if(true){
+  //true 면 C1이 실행
+  console.log('C1');
+} else {
+  //false 면 C2가 실행
+  console.log('C2');
+}
+console.log('D');
