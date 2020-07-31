362d1636a56872e3fbb1ed392b337b3c7ee5fab3
leejaek <67036362+leejaek@users.noreply.github.com>
2020-07-31 11:35:58 +0900

    34. App - 글수정 - 수정 링크 생성

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ var fs = require('fs');
 var url = require('url');
 var qs = require('querystring');
 
-function templateHTML(title, list, body){
+function templateHTML(title, list, body, control){
   return `
   <!doctype html>
   <html>
@@ -14,7 +14,7 @@ function templateHTML(title, list, body){
   <body>
     <h1><a href="/">WEB</a></h1>
     ${list}
-    <a href ="/create">create</a>
+    ${control}
     ${body}
   </body>
   </html>
@@ -42,7 +42,9 @@ var app = http.createServer(function(request,response){
             var title = 'Welcome';
             var description = 'Hello, Node.js';
             var list = templateList(filelist);
-            var template = templateHTML(title, list, `<h2>${title}</h2>${description}`);
+            var template = templateHTML(title, list,
+              `<h2>${title}</h2>${description}`,
+              `<a href ="/create">create</a>`);
             response.writeHead(200);
             response.end(template);
         });
@@ -51,7 +53,9 @@ var app = http.createServer(function(request,response){
           fs.readFile(`data/${queryData.id}`, 'utf8', function(err, description){
             var title = queryData.id;
             var list = templateList(filelist);
-            var template = templateHTML(title, list, `<h2>${title}</h2>${description}`);
+            var template = templateHTML(title, list,
+            `<h2>${title}</h2>${description}`,
+            `<a href ="/create">create</a> <a href = "/update?id=${title}">update</a>`);
             response.writeHead(200);
             response.end(template);
             });
@@ -71,7 +75,7 @@ var app = http.createServer(function(request,response){
                   <input type="submit">
                 </p>
               </form>
-              `);
+              `, '');
             response.writeHead(200);
             response.end(template);
         });
@@ -84,10 +88,7 @@ var app = http.createServer(function(request,response){
           var post = qs.parse(body);
           var title = post.title;
           var description = post.description;
-          //사용자가 작성한 데이터를 파일로 저장
           fs.writeFile(`data/${title}`, description, 'utf8', function(err){
-            //사용자가 데이터 전송 이후 redirection을 통해 작성한 페이지로 사용자를 보냄
-            //302: 페이지를 다른 곳으로 리다이렉션 시키는 뜻
             response.writeHead(302, {Location: `/?id=${title}`});
             response.end('success');
           })
