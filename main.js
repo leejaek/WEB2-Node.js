d8573b8292a0592a52f8a73140a9e9a65c6ad2ac
leejaek <67036362+leejaek@users.noreply.github.com>
2020-07-31 11:21:20 +0900

    33. App - 파일생성과 리다이렉션

--- main.js
+++ main.js
@@ -32,9 +32,6 @@ function templateList(filelist){
   return list
 }
 
-//createServer는 nodejs로 웹브라우저가 접속 들어올때마다 callback 함수로 nodejs 호출
-//그 때 함수에 인자를 두개 주는데 request 는 웹브라우저가 요청할 때 보낸 정보들
-//response는 응답할 때 서버에서 전송할 정보들
 var app = http.createServer(function(request,response){
     var _url = request.url;
     var queryData = url.parse(_url, true).query;
@@ -80,23 +77,21 @@ var app = http.createServer(function(request,response){
         });
     } else if(pathname === '/create_process'){
       var body = '';
-      //request.on : 웹브라우저가 post 방식으로 데이터 전송 시에 데이터가 매우 크면 데이터를 한번에 처리하다가
-      //프로그램에 꺼지거나 무리가 올 수 있음 그래서 nodejs에서는 post 방식으로 전송되는 데이터가 많은 경우를
-      //대비해서 아래와 같은 사용방법을 제공하는데 data는 callback으로 처리하여 전체 데이터를 나누어 서버쪽에서 수신할 때마다
-      //callback함수를 호출하도록 하고, function(data)를 이용해서 수신한 정보를 주도록 함
       request.on('data',function(data){
-          body = body + data; //실행할 때마다 body에 data를 추가함
+          body = body + data;
       });
-      //정보가 들어오다가 더이상 들어올 정보가 없으면 end 뒤에 붙는 function()을 호출하도록 되어있음
       request.on('end',function(){
-        //qureyString 중 parse 라는 함수에 그동안 저장한 body 값을 주면 post data에 들어가도록 함
-        //parse 함수는 정보를 객체화 하는 역할
           var post = qs.parse(body);
           var title = post.title;
           var description = post.description;
+          //사용자가 작성한 데이터를 파일로 저장
+          fs.writeFile(`data/${title}`, description, 'utf8', function(err){
+            //사용자가 데이터 전송 이후 redirection을 통해 작성한 페이지로 사용자를 보냄
+            //302: 페이지를 다른 곳으로 리다이렉션 시키는 뜻
+            response.writeHead(302, {Location: `/?id=${title}`});
+            response.end('success');
+          })
       });
-      response.writeHead(200);
-      response.end('success');
     }else {
       response.writeHead(404);
       response.end('Not Found');
