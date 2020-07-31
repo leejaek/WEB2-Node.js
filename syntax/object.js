0bcbac606ce305528816f09785df6726b3a64726
leejaek <67036362+leejaek@users.noreply.github.com>
2020-07-31 12:35:05 +0900

    39. JavaScript - 객체의 형식

---
+++ syntax/object.js
@@ -0,0 +1,10 @@
+var member = ['eoing', 'k8805', 'hoya'];
+console.log(member[1]); //k8805
+
+var roles = {
+  'programmer':'egoing',
+  'designer' : 'k8805',
+  'manager': 'hoya'
+} //object literal 은 {} ' ' : ' '로 구성
+
+console.log(roles.designer); //k8805
