2e7f76f09d26b678079e43722ce4274c83866aae
leejaek <67036362+leejaek@users.noreply.github.com>
2020-07-31 12:43:41 +0900

    40. JavaScript - 객체의 반복

--- syntax/object.js
+++ syntax/object.js
@@ -1,6 +1,13 @@
 var member = ['eoing', 'k8805', 'hoya'];
 console.log(member[1]); //k8805
 
+var i = 0;
+
+while(i < member.length){
+  console.log('array loop', member[i]);
+  i = i + 1;
+}
+
 var roles = {
   'programmer':'egoing',
   'designer' : 'k8805',
@@ -8,3 +15,8 @@ var roles = {
 } //object literal 은 {} ' ' : ' '로 구성
 
 console.log(roles.designer); //k8805
+console.log(roles['designer']); //k8805
+
+for(var a in roles){
+  console.log('object =>', a, 'value =>', roles[a]);
+}
